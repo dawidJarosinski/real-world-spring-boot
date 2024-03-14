9340c1c8d8807971402536a48c862882467beb42
Dawid Jarosinski <djarosinski03@gmail.com>
2024-03-14 18:05:28 +0100

    initial commit

---
+++ src/main/java/org/example/realworldspringboot/RealWorldSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.realworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/realworldspringboot/RealWorldSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.realworldspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
